393e06a5cee5afba995e2f826ef8a3e39704481d
implicitdef <implicitdef@users.noreply.github.com>
2026-09-01 11:25:23 +0300

    tweak dev favicon

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import { RouterProvider } from "@tanstack/react-router";
 import "./index.css";
 import { router } from "./router";
 
+if (import.meta.env.DEV) {
+  const favicon = document.querySelector<HTMLLinkElement>('link[rel="icon"]');
+  if (favicon) favicon.href = `${import.meta.env.BASE_URL}favicon-dev.svg`;
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
